3381430864c678d4ef7b6d1007f2314837f7bd99
Heiko Scherrer <hscherrer@interface21.io>
2019-08-30 19:17:46 +0200

    #5 Fixed tests and ensured transactional behavior

---
+++ src/main/java/org/openwms/tms/impl/state/AmqpStateListener.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2005-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.tms.impl.state;
+
+import org.ameba.annotation.Measured;
+import org.ameba.exception.ServiceLayerException;
+import org.openwms.core.SpringProfiles;
+import org.openwms.tms.Message;
+import org.openwms.tms.StateChangeException;
+import org.openwms.tms.TransportOrder;
+import org.openwms.tms.TransportOrderState;
+import org.openwms.tms.TransportationService;
+import org.openwms.tms.api.requests.state.StateChangeResponse;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * An AmqpStateListener listens on a start response from remote services as a reply to a former start request, validates the response and
+ * delegates to the {@link Startable} instance for final starting of the {@code TransportOrder}.
+ *
+ * @author Heiko Scherrer
+ */
+@Profile({SpringProfiles.ASYNCHRONOUS_PROFILE})
+@Component
+class AmqpStateListener {
+
+    private final Startable starter;
+    private final TransportationService<TransportOrder> service;
+
+    AmqpStateListener(Startable starter, TransportationService<TransportOrder> service) {
+        this.starter = starter;
+        this.service = service;
+    }
+
+    /**
+     * Listen and validate {@link StateChangeResponse}s.
+     *
+     * @param response The response
+     */
+    @Measured
+    @RabbitListener(queues = "${owms.requests.tms.to.queue-name}")
+    public void onResponse(StateChangeResponse response) {
+
+        try {
+            if (!response.hasRequest()) {
+                throw new ServiceLayerException("Got a response that is assigned to a request: " + response);
+            }
+
+            if (response.hasError()) {
+                TransportOrder to = service.findByPKey(response.getRequest().getTransportOrderPkey());
+                to.setProblem(new Message.Builder()
+                        .withMessage(response.getError().getMessage())
+                        .withOccurred(response.getError().getOccurred())
+                        .build());
+                service.update(to);
+                return;
+            }
+
+            // We only know about the states declared in TransportOrderState!
+            if (TransportOrderState.STARTED.name().equals(response.getAcceptedState())) {
+
+                // Okay the one and only voter accepted to start the TO. We could also have more than one voter but then the implementation
+                // needs to be adjusted.
+                this.starter.start(response.getRequest().getTransportOrderPkey());
+            } else {
+                throw new ServiceLayerException("Got a StateChangeResponse that is not supported: " + response);
+            }
+        } catch (StateChangeException sce) {
+            // fine here
+        } catch (Exception ex) {
+            throw new AmqpRejectAndDontRequeueException(ex.getMessage(), ex);
+        }
+    }
+}
---
+++ src/test/java/org/openwms/tms/TestStateAcceptor.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2005-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.tms;
+
+import org.ameba.annotation.Measured;
+import org.openwms.core.SpringProfiles;
+import org.openwms.tms.api.requests.state.StateChangeRequest;
+import org.openwms.tms.api.requests.state.StateChangeResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * A TestStateAcceptor.
+ *
+ * @author Heiko Scherrer
+ */
+@Profile({SpringProfiles.ASYNCHRONOUS_PROFILE})
+@Component
+class TestStateAcceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestStateAcceptor.class);
+    private final AmqpTemplate amqpTemplate;
+    private final String exchangeName = "tms.requests";
+
+    TestStateAcceptor(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    @Measured
+    @RabbitListener(queues = "test-tms-requests-queue")
+    public void onRequest(StateChangeRequest request) {
+        if ("STARTED".equals(request.getRequestedState())) {
+            LOGGER.debug("STARTING is approved!");
+            amqpTemplate.convertAndSend(exchangeName, "response.state.change", new StateChangeResponse(request, "STARTED", null));
+        }
+    }
+}
