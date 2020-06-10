0018054c6d22fc5b7022d44991714c60c4ac3fdf
Heiko Scherrer <hscherrer@interface21.io>
2020-06-10 22:38:12 +0200

    Enable AspectJ for weaving classes not only interfaces

--- src/main/java/org/openwms/tms/app/TransportationModuleConfiguration.java
+++ src/main/java/org/openwms/tms/app/TransportationModuleConfiguration.java
@@ -37,6 +37,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -57,6 +58,7 @@ import java.util.Locale;
 @Configuration
 @EnableCaching
 @EnableSpringConfigured
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackageClasses = TransportOrderRepository.class)
 @EnableAspects(propagateRootCause = true)
