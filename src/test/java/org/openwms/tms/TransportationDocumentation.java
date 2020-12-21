f5cf54df98a9c035dea6b132dcc9fe5f438dadcc
Heiko Scherrer <hscherrer@interface21.io>
2020-12-21 23:55:25 +0100

    #13 Added more API tests

---
+++ src/test/java/org/openwms/tms/TransportationDocumentation.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2005-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.tms;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openwms.tms.api.TMSApi;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * A TransportationDocumentation.
+ *
+ * @author Heiko Scherrer
+ */
+class TransportationDocumentation {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation, WebApplicationContext context) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+    @Test
+    void shall_find_one_for_barcode() throws Exception {
+        mockMvc.perform(get(TMSApi.TRANSPORT_ORDERS )
+                .param("barcode", "4712")
+                .param("state", "STARTED"))
+                .andExpect(status().isOk())
+                .andDo(document("to-get-bybarcodestate"))
+        ;
+    }
+}
